e4ebdaa522461f359f9f1dd02bec544233d66c1c
wzqwtt <1722249371@qq.com>
2022-07-23 20:27:16 +0800

    Front to Nacos

--- front/src/main/java/com/wzq/front/FrontApplication.java
+++ front/src/main/java/com/wzq/front/FrontApplication.java
@@ -2,7 +2,9 @@ package com.wzq.front;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class FrontApplication {
 
